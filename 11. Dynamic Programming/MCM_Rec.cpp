04b668d26fcf2580c57184255d571e66b4eff3ac
anilpankaj <anilpankaj3@gmail.com>
2022-08-18 17:11:52 +0530

    matrix chain multiplication rec approach

---
+++ 11. Dynamic Programming/MCM_Rec.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int arr[],int i, int j, vector<vector<int>>&t){
+    // base condition
+    if(i>=j){
+        return 0;
+    }
+    int mn = INT_MAX;
+
+    for(int k=i;k<=j-1;k++){
+        int tempans = solve(arr,i,k,t) + solve(arr,k+1,j,t) + arr[i-1]*arr[k]*arr[j];
+        if(tempans<mn){
+            mn = tempans;
+        }
+    }
+    return mn;
+}
+int MatrixChainMultiplication(int arr[], int n){
+    vector<vector<int>>t(n+1,vector<int>(n+1,-1)); // intialize 2d matrix with -1;
+    return solve(arr,1,n-1,t);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<MatrixChainMultiplication(arr,n)<<endl;
+    return 0;   
+}
