cca35ba569171bd74b4756f43edcb60bb93e0b92
anilpankaj <anilpankaj3@gmail.com>
2022-08-18 17:16:19 +0530

    matrix chain multiplication optimization

--- 11. Dynamic Programming/MCM_OPTI.cpp
+++ 11. Dynamic Programming/MCM_OPTI.cpp
@@ -1,23 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(int arr[],int i, int j){
+int solve(int arr[],int i, int j, vector<vector<int>>&t){
     // base condition
     if(i>=j){
         return 0;
     }
+
+    if(t[i][j]!=-1){
+        return t[i][j];
+    }
+
     int mn = INT_MAX;
 
     for(int k=i;k<=j-1;k++){
-        int tempans = solve(arr,i,k) + solve(arr,k+1,j) + arr[i-1]*arr[k]*arr[j];
+        int tempans = solve(arr,i,k,t) + solve(arr,k+1,j,t) + arr[i-1]*arr[k]*arr[j];
         if(tempans<mn){
             mn = tempans;
         }
     }
-    return mn;
+    t[i][j] = mn;
+    return t[i][j];
 }
 int MatrixChainMultiplication(int arr[], int n){
-    return solve(arr,1,n-1);
+    vector<vector<int>>t(n+1,vector<int>(n+1,-1)); // intialize 2d matrix with -1;
+    return solve(arr,1,n-1,t);
 }
 
 int main(){
